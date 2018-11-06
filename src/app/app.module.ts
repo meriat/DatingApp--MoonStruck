7b7a9e39588643c55c6e9801ccae6d2db42e5951
connor <connormccarthy8@gmail.com>
2018-11-06 13:00:18 -0800

    add chat components, user class, message class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,16 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ChatComponent } from './pages/chat/chat.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatInputComponent } from './pages/chat/components/chat-input/chat-input.component';
+import { ChatroomListComponent } from './pages/chat/components/chatroom-list/chatroom-list.component';
+import { ChatroomTitleBarComponent } from './pages/chat/components/chatroom-title-bar/chatroom-title-bar.component';
+import { ChatMessagesComponent } from './pages/chat/components/chat-messages/chat-messages.component';
+import { ChatroomWindowComponent } from './pages/chat/components/chatroom-window/chatroom-window.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent, 
-    SignupComponent, ChatComponent, NavbarComponent
+    SignupComponent, ChatComponent, NavbarComponent, ChatInputComponent, ChatroomListComponent, ChatroomTitleBarComponent, ChatMessagesComponent, ChatroomWindowComponent
   ],
   imports: [
     BrowserModule,
