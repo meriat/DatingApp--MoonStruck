105c4b9a64da62bd5cd0f8ea140719b31f11cc99
connor <connormccarthy8@gmail.com>
2018-11-06 11:44:11 -0800

    add sign up and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/login'},
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: '**', redirectTo:'/login'}
 ];
 
 // @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app-routing.module';
 
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import { SignupComponent } from './pages/signup/signup.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent, 
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
