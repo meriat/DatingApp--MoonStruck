7b503604ffe4ab1d08e0247462723c56328698dd
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-11-08 11:21:43 -0800

    fixed pull

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -1,18 +1,34 @@
-import { Component} from '@angular/core';
-
-
-@Component({
+import { Component, OnInit} from '@angular/core';
+import { SurveyComponent } from '../survey/survey.component'
+ @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.css']
-})
+ })
 
-export class MatchesComponent {
-  constructor() {
-    
+ export class MatchesComponent {
 
-  }
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+   
+  ngOnInit(){}
  
   
-
-}
+ toggle() {
+    this.show = !this.show;
+     if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+   text = '';
+  openFirstImg() {
+  this.text = '' 
+  }
+  openSecondImg(){
+  this.text = ''
+  }
+  openThirdImg(){
+    this.text= ''
+  }
+ }
